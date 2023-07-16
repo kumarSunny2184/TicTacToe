a49cf7f0a9fa2d0b030196ef0de0a3c0df8b9d8f
sunny kumar <kumarsunnysid343@gmail.com>
2023-07-17 00:12:56 +0530

    displaying x when clicking

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
 };
 
 Square.propTypes = {
-  value: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Square;
